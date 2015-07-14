a87d284c51977f8a810023ce4a1604320e74de50
hhurz <hhurz@online.de>
2015-07-14 14:44:16 +0200

    Refactoring, .bind() replaced by .on()

--- jquery.ui.menubar.js
+++ jquery.ui.menubar.js
@@ -22,110 +22,164 @@ $.widget( "ui.menubar", {
       }
     },
     _create : function () {
-      var b = this;
-      this.menuItems = this.element.children(this.options.items),
-      this.items = this.menuItems.children("button, a"),
-      this.menuItems.addClass("ui-menubar-item").attr("role", "presentation"),
-      this.items.slice(1).attr("tabIndex", -1),
-      this.element.addClass("ui-menubar ui-widget-header ui-helper-clearfix").attr("role", "menubar"),
-      this._focusable(this.items),
-      this._hoverable(this.items),
-      this.items.siblings(this.options.menuElement).menu({
-        position : {
-          within : this.options.position.within
-        },
-        select : function (c, d) {
-          d.item.parents("ul.ui-menu:last").hide(),
-          b._close(),
-          a(c.target).prev().focus(),
-          b._trigger("select", c, d)
-        },
-        menus : b.options.menuElement
-      }).hide().attr({
-        "aria-hidden" : "true",
-        "aria-expanded" : "false"
-      }).bind("keydown.menubar", function (c) {
-        var d = a(this);
-        if (d.is(":hidden"))
-          return;
-        switch (c.keyCode) {
-        case a.ui.keyCode.LEFT:
-          b.previous(c),
-          c.preventDefault();
-          break;
-        case a.ui.keyCode.RIGHT:
-          b.next(c),
-          c.preventDefault()
+      var that = this;
+      this.menuItems = this.element.children(this.options.items);
+      this.items = this.menuItems.children("button, a");
+      this.menuItems.addClass("ui-menubar-item").attr("role", "presentation");
+      // let only the first item receive focus
+      this.items.slice(1).attr("tabIndex", -1);
+
+      this.element
+              .addClass("ui-menubar ui-widget-header ui-helper-clearfix")
+              .attr("role", "menubar");
+      this._focusable(this.items);
+      this._hoverable(this.items);
+      this.items.siblings(this.options.menuElement)
+              .menu({
+                position : {
+                  within : this.options.position.within
+                },
+                select : function (event, ui) {
+                  ui.item.parents("ul.ui-menu:last").hide();
+                  that._close();
+                  // TODO what is this targetting? there's probably a better way to access it
+                  $(event.target).prev().focus();
+                  that._trigger("select", event, ui);
+                },
+                menus : that.options.menuElement
+              })
+              .hide().attr({
+                "aria-hidden" : "true",
+                "aria-expanded" : "false"
+              })
+              .on("keydown.menubar", function (event) {
+                var menu = $(this);
+                if (menu.is(":hidden"))
+                  return;
+                switch (event.keyCode) {
+                case $.ui.keyCode.LEFT:
+                  that.previous(event);
+                  c.preventDefault();
+                  break;
+                case $.ui.keyCode.RIGHT:
+                  that.next(event);
+                  event.preventDefault();
+                  break;
+                }
+              });
+      
+        this.items.each(function () {
+              var input = $(this),
+                  // TODO menu var is only used on two places, doesn't quite justify the .each
+                  menu = input.next(that.options.menuElement);
+
+              input.on("click.menubar focus.menubar mouseenter.menubar", function (event) {
+                if (event.type == "focus" && !event.originalEvent)
+                  return;
+                event.preventDefault();
+
+                // TODO can we simplify or extractthis check? especially the last two expressions
+                // there's a similar active[0] == menu[0] check in _open
+                if (event.type == "click" && menu.is(":visible") && that.active && that.active[0] == menu[0]) {
+                  that._close();
+                  return;
+                }
+                if ((that.open && event.type == "mouseenter") || event.type == "click" || that.options.autoExpand) {
+                  if ( that.options.autoExpand ) 
+                    clearTimeout(that.closeTimer); 
+                  that._open(event, menu);
+                }
+        })
+        .on("keydown", function (event) {
+              switch (event.keyCode) {
+              case $.ui.keyCode.SPACE:
+              case $.ui.keyCode.UP:
+              case $.ui.keyCode.DOWN:
+                that._open(event, $(this).next());
+                event.preventDefault();
+                break;
+              case $.ui.keyCode.LEFT:
+                that.previous(event, $(this));
+                event.preventDefault();
+                break;
+              case $.ui.keyCode.RIGHT:
+                that.next(event, $(this));
+                event.preventDefault();
+                break;
+              }
+        })
+        .addClass("ui-button ui-widget ui-button-text-only ui-menubar-link")
+        .attr("role", "menuitem")
+        .attr("aria-haspopup", "true")
+        .wrapInner("<span class='ui-button-text'></span>");
+        
+        // TODO review if these options are a good choice, maybe they can be merged
+        if ( that.options.menuIcon ) {
+          input.addClass("ui-state-default").append("<span class='ui-button-icon-secondary ui-icon ui-icon-triangle-1-s'></span>");
+          input.removeClass("ui-button-text-only").addClass("ui-button-text-icon-secondary");
         }
-      }),
-      this.items.each(function () {
-        var c = a(this),
-        d = c.next(b.options.menuElement);
-        c.bind("click.menubar focus.menubar mouseenter.menubar", function (a) {
-          if (a.type == "focus" && !a.originalEvent)
-            return;
-          a.preventDefault();
-          if (a.type == "click" && d.is(":visible") && b.active && b.active[0] == d[0]) {
-            b._close();
-            return
-          }
-          if (b.open && a.type == "mouseenter" || a.type == "click" || b.options.autoExpand)
-            b.options.autoExpand && clearTimeout(b.closeTimer), b._open(a, d)
-        }).bind("keydown", function (c) {
-          switch (c.keyCode) {
-          case a.ui.keyCode.SPACE:
-          case a.ui.keyCode.UP:
-          case a.ui.keyCode.DOWN:
-            b._open(c, a(this).next()),
-            c.preventDefault();
-            break;
-          case a.ui.keyCode.LEFT:
-            b.previous(c),
-            c.preventDefault();
-            break;
-          case a.ui.keyCode.RIGHT:
-            b.next(c),
-            c.preventDefault()
-          }
-        }).addClass("ui-button ui-widget ui-button-text-only ui-menubar-link").attr("role", "menuitem").attr("aria-haspopup", "true").wrapInner("<span class='ui-button-text'></span>"),
-        b.options.menuIcon && (c.addClass("ui-state-default").append("<span class='ui-button-icon-secondary ui-icon ui-icon-triangle-1-s'></span>"), c.removeClass("ui-button-text-only").addClass("ui-button-text-icon-secondary")),
-        b.options.buttons || c.addClass("ui-menubar-link").removeClass("ui-state-default")
-      }),
-      b._bind({
-        keydown : function (c) {
-          if (c.keyCode == a.ui.keyCode.ESCAPE && b.active && b.active.menu("collapse", c) !== !0) {
-            var d = b.active;
-            b.active.blur(),
-            b._close(c),
-            d.prev().focus()
-          }
-        },
-        focusin : function (a) {
-          clearTimeout(b.closeTimer)
-        },
-        focusout : function (a) {
-          b.closeTimer = setTimeout(function () {
-              b._close(a)
-            }, 150)
-        },
-        "mouseleave .ui-menubar-item" : function (a) {
-          b.options.autoExpand && (b.closeTimer = setTimeout(function () {
-                b._close(a)
-              }, 150))
-        },
-        "mouseenter .ui-menubar-item" : function (a) {
-          clearTimeout(b.closeTimer)
+        
+        if ( that.options.buttons ) {
+          // TODO ui-menubar-link is added above, not needed here?
+          input.addClass("ui-menubar-link").removeClass("ui-state-default");
         }
-      })
+      });
+      that._on({
+        keydown : function (event) {
+          if (event.keyCode == $.ui.keyCode.ESCAPE && that.active && that.active.menu("collapse", event) !== true) {
+            var active = that.active;
+            that.active.blur();
+            that._close(event);
+            active.prev().focus();
+          }
+        },
+        focusin : function (event) {
+          clearTimeout(that.closeTimer);
+        },
+        focusout : function (event) {
+          that.closeTimer = setTimeout(function () {
+              that._close(event);
+            }, 100);
+        },
+        "mouseleave .ui-menubar-item" : function (event) {
+          if ( that.options.autoExpand ) { 
+            that.closeTimer = setTimeout(function () {
+                that._close(event);
+              }, 100);
+          }
+        },
+        "mouseenter .ui-menubar-item" : function (event) {
+          clearTimeout(that.closeTimer);
+        }
+      });
     },
     _destroy : function () {
-      this.menuItems.removeClass("ui-menubar-item").removeAttr("role"),
-      this.element.removeClass("ui-menubar ui-widget-header ui-helper-clearfix").removeAttr("role").unbind(".menubar"),
-      this.items.unbind(".menubar").removeClass("ui-button ui-widget ui-button-text-only ui-menubar-link ui-state-default").removeAttr("role").removeAttr("aria-haspopup").children("span.ui-button-text").each(function (b, c) {
-        var d = a(this);
-        d.parent().html(d.html())
-      }).end().children(".ui-icon").remove(),
-      this.element.find(":ui-menu").menu("destroy").show().removeAttr("aria-hidden").removeAttr("aria-expanded").removeAttr("tabindex").unbind(".menubar")
+      this.menuItems
+              .removeClass("ui-menubar-item")
+              .removeAttr("role");
+      this.element
+              .removeClass("ui-menubar ui-widget-header ui-helper-clearfix")
+              .removeAttr("role")
+              .off(".menubar");
+      this.items
+              .off(".menubar")
+              .removeClass("ui-button ui-widget ui-button-text-only ui-menubar-link ui-state-default")
+              .removeAttr("role")
+              .removeAttr("aria-haspopup")
+              .children("span.ui-button-text").each(function (i, e) {
+                      var item = $(this);
+                      item.parent().html(item.html());
+              })
+              .end()
+              .children(".ui-icon").remove();
+      
+      this.element.find(":ui-menu")
+              .menu("destroy")
+              .show()
+              .removeAttr("aria-hidden")
+              .removeAttr("aria-expanded")
+              .removeAttr("tabindex")
+              .off(".menubar");
     },
     _close : function () {
       if (!this.active || !this.active.length)
@@ -133,35 +187,67 @@ $.widget( "ui.menubar", {
       this.active.menu("collapseAll").hide().attr({
         "aria-hidden" : "true",
         "aria-expanded" : "false"
-      }),
-      this.active.prev().removeClass("ui-state-active").removeAttr("tabIndex"),
-      this.active = null,
-      this.open = false
+      });
+      this.active.prev().removeClass("ui-state-active").removeAttr("tabIndex");
+      this.active = null;
+      this.open = false;
     },
-    _open : function (b, c) {
-      if (this.active && this.active[0] == c[0])
+    _open : function (event, menu) {
+      // on a single-button menubar, ignore reopening the same menu
+      if (this.active && this.active[0] == menu[0])
         return;
-      this.active && (this.active.menu("collapseAll").hide().attr({
-          "aria-hidden" : "true",
-          "aria-expanded" : "false"
-        }), this.active.prev().removeClass("ui-state-active"));
-      var d = c.prev().addClass("ui-state-active").attr("tabIndex", -1);
-      this.active = c.show().position(a.extend({
-            of : d
-          }, this.options.position)).removeAttr("aria-hidden").attr("aria-expanded", "true").menu("focus", b, c.children(".ui-menu-item").first()).focus().focusin(),
-      this.open = !0
+        
+      if ( this.active ) {
+        // TODO refactor, almost the same as _close above, but don't remove tabIndex
+        this.active.menu("collapseAll").hide().attr({
+        "aria-hidden" : "true",
+        "aria-expanded" : "false"
+        }); 
+        this.active.prev().removeClass("ui-state-active");
+      }
+      // set tabIndex -1 to have the button skipped on shift-tab when menu is open (it gets focus)
+      var button = menu.prev().addClass("ui-state-active").attr("tabIndex", -1);
+      this.active = menu.show().position($.extend({
+            of : button
+          }, this.options.position))
+          .removeAttr("aria-hidden")
+          .attr("aria-expanded", "true")
+          .menu("focus", event, menu.children(".ui-menu-item").first())
+          // TODO need a comment here why both events are triggered
+          .focus()
+          .focusin();
+      this.open = true;
     },
-    next : function (a) {
-      this._move("next", "first", a)
+    next : function (event) {
+      this._move("next", "first", event);
     },
-    previous : function (a) {
-      this._move("prev", "last", a)
+    previous : function (event) {
+      this._move("prev", "last", event);
     },
-    _move : function (b, c, d) {
-      var e,
-      f;
-      this.open ? (e = this.active.closest(".ui-menubar-item")[b + "All"](this.options.items).first().children(".ui-menu").eq(0), f = this.menuItems[c]().children(".ui-menu").eq(0)) : d ? (e = a(d.target).closest(".ui-menubar-item")[b + "All"](this.options.items).children(".ui-menubar-link").eq(0), f = this.menuItems[c]().children(".ui-menubar-link").eq(0)) : e = f = this.menuItems.children("a").eq(0),
-      e.length ? this.open ? this._open(d, e) : e.removeAttr("tabIndex")[0].focus() : this.open ? this._open(d, f) : f.removeAttr("tabIndex")[0].focus()
+    _move : function (b, c, event) {
+      var e, f;
+      if ( this.open ) {
+        e = this.active.closest(".ui-menubar-item")[b + "All"](this.options.items).first().children(".ui-menu").eq(0);
+        f = this.menuItems[c]().children(".ui-menu").eq(0);
+      }
+      else if ( event ) {
+        e = $(event.target).closest(".ui-menubar-item")[b + "All"](this.options.items).children(".ui-menubar-link").eq(0);
+        f = this.menuItems[c]().children(".ui-menubar-link").eq(0);
+      }
+      else
+        e = f = this.menuItems.children("a").eq(0);
+      if ( e.length ) {
+        if ( this.open ) 
+          this._open(event, e);
+        else
+          e.removeAttr("tabIndex")[0].focus();
+      }
+      else {
+        if ( this.open ) 
+          this._open(event, f);
+        else
+          f.removeAttr("tabIndex")[0].focus();
+      }
     }
 });
 
